4c0d75266dd3e9482e11d91da84b5724205fd1bf
Jimmi Holst Christensen <jhc@dismail.de>
2019-12-24 03:56:22 +0900

    Paragraph does not auto space

--- README.sh
+++ README.sh
@@ -3,11 +3,11 @@
 src/header 'Shelldown'
 
 src/paragraph \
-'Shelldown is a set of scripts that make your shell into a markup' \
-'language. In fact, this README was generated with Shelldown!' \
-'See' -c 'README.sh' '.'
+'Shelldown is a set of scripts that make your shell into a markup ' \
+'language. In fact, this README was generated with Shelldown! ' \
+'See ' -c 'README.sh' '.'
 
 src/paragraph \
-'Shelldown currently supports outputting html, markdown and to' \
-'the terminal with ansi escapes. The output is determined by' \
-'the' -c 'SD_LANG' ' environment variable.'
+'Shelldown currently supports outputting html, markdown and to ' \
+'the terminal with ansi escapes. The output is determined by ' \
+'the ' -c 'SD_LANG' ' environment variable.'
