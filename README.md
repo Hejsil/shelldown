49ee03fb5f7233bdee737a0b7181369ee497230e
Jimmi Holst Christensen <jhc@dismail.de>
2019-12-23 10:02:04 +0900

    Adds code, header, hr and paragraph
    Also generates a readme with shelldown

---
+++ README.md
@@ -0,0 +1,6 @@
+# Shelldown
+
+Shelldown is a set of scripts that make your shell into a markup language. In fact, this README was generated with Shelldown! See `README.sh`.
+
+Shelldown currently supports outputting html, markdown and to the terminal with ansi escapes. The output is determined by the `SD_LANG` environment variable.
+
---
+++ README.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+src/header 'Shelldown'
+
+src/paragraph \
+'Shelldown is a set of scripts that make your shell into a markup' \
+'language. In fact, this README was generated with Shelldown!' \
+'See' -c 'README.sh' '.'
+
+src/paragraph \
+'Shelldown currently supports outputting html, markdown and to' \
+'the terminal with ansi escapes. The output is determined by' \
+'the' -c 'SD_LANG' ' environment variable.'
